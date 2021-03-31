a3d2e66b9ade763a755c5712fd7979afd8f52dee
C-H Han <itq1@naver.com>
2021-03-31 13:48:25 +0900

    Anonymous class

--- app/src/main/java/com/androidapp/anonymousclass/MainActivity.java
+++ app/src/main/java/com/androidapp/anonymousclass/MainActivity.java
@@ -18,13 +18,12 @@ public class MainActivity extends AppCompatActivity {
         mButton1 = findViewById(R.id.button1);
         mTextView1 = findViewById(R.id.textView1);
 
-        mButton1.setOnClickListener(new MyOnClickListener());
+        mButton1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                mTextView1.setText("You clicked button 1");
+            }
+        });
     }
 
-    private class MyOnClickListener implements View.OnClickListener {
-        @Override
-        public void onClick(View v) {
-            mTextView1.setText("You clicked button 1");
-        }
-    }
 }
